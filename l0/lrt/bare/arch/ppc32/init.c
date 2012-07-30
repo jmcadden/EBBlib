b70730b5900f84e99ec49d2a0a53089a3163297b
Jim Cadden <jmcadden@bu.edu>
2012-07-30 21:06:55 +0000

    Remapped TLB entries
    The initial TLB entry is overwritten with a small init-only TLB entry.
    The rest of the kernel is mapped in, so this small entry can be removed.

--- l0/lrt/bare/arch/ppc32/init.c
+++ l0/lrt/bare/arch/ppc32/init.c
@@ -41,7 +41,7 @@ clear_bss(void)
     *i = 0;
   }
 }
-/*
+
 int
 ipow(int base, int exp)
 {
@@ -55,92 +55,243 @@ ipow(int base, int exp)
   }
   return result;
 }
-*/
 
-void __attribute__((noreturn))
+
+void __attribute__((section(".init.text"), noreturn))
 init(void) 
 {
-/*
-*  extern uint8_t _vec_start[];
-*  set_ivpr(_vec_start);
-*
-*  //IVORS in the order that fixed registers
-*  // are set
-*  set_spr(SPRN_IVOR1, 0x000);
-*  set_spr(SPRN_IVOR0, 0x100);
-*  set_spr(SPRN_IVOR15, 0x200);
-*  set_spr(SPRN_IVOR2, 0x300);
-*  set_spr(SPRN_IVOR3, 0x400);
-*  set_spr(SPRN_IVOR4, 0x500);
-*  set_spr(SPRN_IVOR5, 0x600);
-*  set_spr(SPRN_IVOR6, 0x700);
-*  set_spr(SPRN_IVOR7, 0x800);
-*  set_spr(SPRN_IVOR8, 0x900);
-*  set_spr(SPRN_IVOR9, 0xA00);
-*  set_spr(SPRN_IVOR10, 0xB00);
-*  set_spr(SPRN_IVOR11, 0xC00);
-*  set_spr(SPRN_IVOR12, 0xD00);
-*  set_spr(SPRN_IVOR13, 0xE00);
-*  set_spr(SPRN_IVOR14, 0xF00);
-*
-*  //setup MSR
-*  msr msr = get_msr();
-*  //enable machine check
-*  msr.me = 1;
-*  //make sure external interrupts are off
-*  msr.ee = 0;
-*  //enable fpu
-*  msr.fp = 1;
-*  set_msr(msr);
-*/
+  extern uint8_t _vec_start[];
+  set_ivpr(_vec_start);
+
+  //IVORS in the order that fixed registers
+  // are set
+  set_spr(SPRN_IVOR0, 0x000);
+  set_spr(SPRN_IVOR1, 0x100);
+  set_spr(SPRN_IVOR2, 0x200);
+  set_spr(SPRN_IVOR3, 0x300);
+  set_spr(SPRN_IVOR4, 0x400);
+  set_spr(SPRN_IVOR5, 0x500);
+  set_spr(SPRN_IVOR6, 0x600);
+  set_spr(SPRN_IVOR7, 0x700);
+  set_spr(SPRN_IVOR8, 0x800);
+  set_spr(SPRN_IVOR9, 0x900);
+  set_spr(SPRN_IVOR10, 0xA00);
+  set_spr(SPRN_IVOR11, 0xB00);
+  set_spr(SPRN_IVOR12, 0xC00);
+  set_spr(SPRN_IVOR13, 0xD00);
+  set_spr(SPRN_IVOR14, 0xE00);
+  set_spr(SPRN_IVOR15, 0xF00);
+  
+  //setup MSR
+  msr msr = get_msr();
+  //enable machine check
+  msr.me = 1;
+  //make sure external interrupts are off
+  msr.ee = 0;
+  //enable fpu
+  msr.fp = 1;
+  set_msr(msr);
+  
   clear_bss();
   stdout = mailbox_init();
   printf("Mailbox initialized\n");
+/*   uint32_t index; */
+/*   extern char initstart[]; */
+/*   extern char initend[]; */
+/*   extern char kend[]; */
+/*   printf("Mailbox initialized\n"); */
+/*   printf("initstart index = %x \n", (uintptr_t)initstart); */
+/*   printf("initend index = %x \n", (uintptr_t)initend); */
+/*   printf("kend index = %x \n", (uintptr_t)kend); */
+/*   printf("Mailbox initialized\n"); */
+
+/*   asm volatile ( */
+/* 		"tlbsx %[index], 0, %[addr]" */
+/* 		: [index] "=r" (index) */
+/* 		: [addr] "r" (initstart) */
+/* 		); */
+/*   printf("initstart index = %d\n", index); */
+/*   asm volatile ( */
+/* 		"tlbsx %[index], 0, %[addr]" */
+/* 		: [index] "=r" (index) */
+/* 		: [addr] "r" (initend) */
+/* 		); */
+/*   printf("initend index = %d\n", index); */
+/*   asm volatile ( */
+/* 		"tlbsx %[index], 0, %[addr]" */
+/* 		: [index] "=r" (index) */
+/* 		: [addr] "r" (kend) */
+/* 		); */
+/*   printf("kend index = %d\n", index); */
+		
+  for (int i = 0; i < 64; i++) {
+    tlb_word_0 w0;
+    tlb_word_1 w1;
+    tlb_word_2 w2;
+    asm volatile (
+  		  "tlbre %[word0],%[entry],0;"
+  		  "tlbre %[word1],%[entry],1;"
+  		  "tlbre %[word2],%[entry],2"
+  		  : [word0] "=&r" (w0.val),
+  		    [word1] "=&r" (w1.val),
+  		    [word2] "=&r" (w2.val)
+  		  : [entry] "r" (i)
+  		  );
+  mmucr mmucr;
+    mmucr.val = get_spr(SPRN_MMUCR);
+    if (w0.v) {
+      printf("TLB Entry %d:\n", i);
+      printf("t0: %x, t1: %x, t2: %x\n", w0.val, w1.val, w2.val);
+      uint32_t size = ipow(4,w0.size); // in Kb
+      uint32_t vaddr = w0.epn << (10);
+      uint32_t vaddr_start = vaddr & (~((1 << (__builtin_ffs(size) + 9)) - 1));
+      uint32_t vaddr_end = vaddr | ((1 << (__builtin_ffs(size) + 9)) - 1);
+      uint64_t paddr = (((uint64_t)w1.erpn) << 32) |
+  	(((uint64_t)w1.rpn) << 10);
+      uint64_t paddr_start = paddr &
+  	(~((1 << (((uint64_t)__builtin_ffs(size)) + 9)) - 1));
+      uint64_t paddr_end = paddr |
+  	((1 << (((uint64_t)__builtin_ffs(size)) + 9)) - 1);
+
+      printf("Virt: %x - %x -> Phys: %llx - %llx\n",
+  	     vaddr_start, vaddr_end,
+  	     paddr_start, paddr_end);
+      printf("TID: %d, TS: %d, Size: %dkb, FAR: %d\n",
+  	     mmucr.stid, w0.ts, size, w2.far);
+      printf("WL1: %d, IL1I: %d, IL1D: %d, IL2I: %d, IL2D: %d\n",
+  	     w2.wl1, w2.il1i, w2.il1d, w2.il2i, w2.il2d);
+      printf("W: %d, I: %d, M: %d, G: %d, E: %d, UX: %d, UW: %d\n",
+  	     w2.w, w2.i, w2.m, w2.g, w2.e, w2.ux, w2.uw);
+      printf("UR: %d, SX: %d, SW: %d, SR: %d\n",
+  	     w2.ur, w2.sx, w2.sw, w2.sr);
+      printf("\n\n");
+    }
+  }
+
+  extern char initstart[];
+  extern char initend[];
+  if (((uintptr_t)initend - (uintptr_t)initstart) > 0x4000) {
+    while (1)
+      ;
+  }
+  int index;
+  asm volatile (
+		"tlbsx %[index], 0, %[addr]"
+		: [index] "=r" (index)
+		: [addr] "r" (initstart)
+		);
+
+  tlb_word_0 t0;
+  tlb_word_1 t1;
+  tlb_word_2 t2;
+  t0.val = 0;
+  t0.epn = ((uintptr_t)initstart) >> 10;
+  t0.v = 1;
+  t0.size = 2;//ilog2( >> 10) / 2;
+
+  t1.val = 0;
+  t1.rpn = (uint32_t)(((uintptr_t)initstart >> 10) & 0x3fffff);
+  t1.erpn =0;// (uint32_t)((initstart >> 32) & 0xf);
+
+  t2.val = 0;
+  t2.wl1 = 1; 
+  t2.m = 1;
+  t2.sx = 1;
+  t2.sw = 1;
+  t2.sr = 1;
+
+  asm volatile (
+		"tlbwe %[word0],%[entry],0;"
+		"tlbwe %[word1],%[entry],1;"
+		"tlbwe %[word2],%[entry],2;"
+		"isync;"
+		:
+		: [word0] "r" (t0.val),
+		  [word1] "r" (t1.val),
+		  [word2] "r" (t2.val),
+		  [entry] "r" (index)
+		);
+  //unmap all other entries
+  for (int i = 0; i < 64; i++) {
+    if (i == index) {
+      continue;
+    }
+    t0.val = 0;
+    asm volatile (
+		  "tlbwe %[word0],%[entry],0;"
+		  "tlbwe %[word1],%[entry],1;"
+		  "tlbwe %[word2],%[entry],2;"
+		  :
+		  : [word0] "r" (t0.val),
+		    [word1] "r" (t1.val),
+		    [word2] "r" (t2.val),
+		    [entry] "r" (i)
+		  );
+  }
+  asm volatile ("isync");
+  for (int i = 0; ((i+1) * 0x4000) <= (uintptr_t)initstart; i++) {
+    if (i == index) {
+      while (1)
+	;
+    }
+    t0.val = 0;
+    t0.epn = ((uintptr_t)i * 0x4000) >> 10;
+    t0.v = 1;
+    t0.size = 2; //16k mapping
+    
+    t1.val = 0;
+    t1.rpn = (uint32_t)(((uintptr_t)(i * 0x4000) >> 10) & 0x3fffff);
+    t1.erpn =0;
+    
+    t2.val = 0;
+    t2.wl1 = 1; 
+    t2.m = 1;
+    t2.sx = 1;
+    t2.sw = 1;
+    t2.sr = 1;
+    asm volatile (
+		  "tlbwe %[word0],%[entry],0;"
+		  "tlbwe %[word1],%[entry],1;"
+		  "tlbwe %[word2],%[entry],2;"
+		  :
+		  : [word0] "r" (t0.val),
+		    [word1] "r" (t1.val),
+		    [word2] "r" (t2.val),
+		    [entry] "r" (i)
+		  );
+  }
+  asm volatile("isync");
+  //Map in mailbox
+  uint64_t mb_phys = 0x7ffffc000LL; //FIXME: get from FDT
+  uint32_t mb_virt = 0xffffc000; //allocate somehow
+  t0.val = 0;
+  t0.epn = (uint32_t)(mb_virt >> 10);
+  t0.v = 1;
+  t0.size = 2;//ilog2( >> 10) / 2;
+
+  t1.val = 0;
+  t1.rpn = (uint32_t)((mb_phys >> 10) & 0x3fffff);
+  t1.erpn = (uint32_t)((mb_phys >> 32) & 0xf);
+
+  t2.val = 0;
+  t2.wl1 = 1;
+  t2.sx = 1;
+  t2.sw = 1;
+  t2.sr = 1;
+
+  asm volatile (
+		"tlbwe %[word0],%[entry],0;"
+		"tlbwe %[word1],%[entry],1;"
+		"tlbwe %[word2],%[entry],2;"
+		"isync;"
+		:
+		: [word0] "r" (t0.val),
+		  [word1] "r" (t1.val),
+		  [word2] "r" (t2.val),
+		  [entry] "r" (19) //FIXME
+		);  
+  printf("Success!\n");
   while(1);
-
-  /* for (int i = 0; i < 64; i++) { */
-  /*   tlb_word_0 w0; */
-  /*   tlb_word_1 w1; */
-  /*   tlb_word_2 w2; */
-  /*   asm volatile ( */
-  /* 		  "tlbre %[word0],%[entry],0;" */
-  /* 		  "tlbre %[word1],%[entry],1;" */
-  /* 		  "tlbre %[word2],%[entry],2" */
-  /* 		  : [word0] "=&r" (w0.val), */
-  /* 		    [word1] "=&r" (w1.val), */
-  /* 		    [word2] "=&r" (w2.val) */
-  /* 		  : [entry] "r" (i) */
-  /* 		  ); */
-  /* mmucr mmucr; */
-  /*   mmucr.val = get_spr(SPRN_MMUCR); */
-  /*   if (w0.v) { */
-  /*     printf("TLB Entry %d:\n", i); */
-  /*     printf("t0: %x, t1: %x, t2: %x\n", w0.val, w1.val, w2.val); */
-  /*     uint32_t size = ipow(4,w0.size); // in Kb */
-  /*     uint32_t vaddr = w0.epn << (10); */
-  /*     uint32_t vaddr_start = vaddr & (~((1 << (__builtin_ffs(size) + 9)) - 1)); */
-  /*     uint32_t vaddr_end = vaddr | ((1 << (__builtin_ffs(size) + 9)) - 1); */
-  /*     uint64_t paddr = (((uint64_t)w1.erpn) << 32) | */
-  /* 	(((uint64_t)w1.rpn) << 10); */
-  /*     uint64_t paddr_start = paddr &  */
-  /* 	(~((1 << (((uint64_t)__builtin_ffs(size)) + 9)) - 1)); */
-  /*     uint64_t paddr_end = paddr | */
-  /* 	((1 << (((uint64_t)__builtin_ffs(size)) + 9)) - 1); */
-
-  /*     printf("Virt: %x - %x -> Phys: %llx - %llx\n", */
-  /* 	     vaddr_start, vaddr_end, */
-  /* 	     paddr_start, paddr_end); */
-  /*     printf("TID: %d, TS: %d, Size: %dkb, FAR: %d\n", */
-  /* 	     mmucr.stid, w0.ts, size, w2.far); */
-  /*     printf("WL1: %d, IL1I: %d, IL1D: %d, IL2I: %d, IL2D: %d\n", */
-  /* 	     w2.wl1, w2.il1i, w2.il1d, w2.il2i, w2.il2d); */
-  /*     printf("W: %d, I: %d, M: %d, G: %d, E: %d, UX: %d, UW: %d\n", */
-  /* 	     w2.w, w2.i, w2.m, w2.g, w2.e, w2.ux, w2.uw); */
-  /*     printf("UR: %d, SX: %d, SW: %d, SR: %d\n", */
-  /* 	     w2.ur, w2.sx, w2.sw, w2.sr); */
-  /*     printf("\n\n"); */
-  /*   } */
-  /* } */
   
 //  lrt_pic_init(lrt_start_isr);
 }
