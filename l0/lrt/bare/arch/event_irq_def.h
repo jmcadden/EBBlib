1d99cff4c54941480512ebc6c4eff3d755fa136e
Jim Cadden <jmcadden@bu.edu>
2012-08-29 21:49:03 +0000

    Event-driven tree flush!

---
+++ l0/lrt/bare/arch/event_irq_def.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2012 by Project SESA, Boston University
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef __LRT_EVENT_IRQ_DEF_H__
+#error "This file must be included from l0/lrt/event_irq_def.h"
+#endif
+
+#if ARCH_AMD64
+#include <l0/lrt/bare/arch/amd64/event_irq_def.h>
+#elif ARCH_PPC
+#include <l0/lrt/bare/arch/ppc32/event_irq_def.h>
+#endif
